85da0fef8dd0cbe29072cd1a1c1ae42d5c442bc6
Ivan Sivakov <Vanya-sivakov@mail.ru>
2025-03-25 21:50:29 +0300

    PrimitivnieTipiDannih

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 1;
+        short x = 2;
+        int v = 3;
+        long m = 4;
+        double d = 0.3;
+        float f = 0.4F;
+        char c = 'x';
+        boolean b = false;
+
+
+
+
+        System.out.println();
+        String s = "Petya";
+    }
+}
